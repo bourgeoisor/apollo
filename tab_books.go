598628ee88d6eb735e0929403b30071bff977bcf
Olivier Bourgeois <bourgeoisor@gmail.com>
2015-08-18 18:12:01 -0300

    Added book tagging with Google Books

--- tab_books.go
+++ tab_books.go
@@ -2,10 +2,11 @@ package main
 
 import (
     "github.com/nsf/termbox-go"
-    //"encoding/json"
-    //"net/http"
-    //"io/ioutil"
-    //"strings"
+    "encoding/json"
+    "net/http"
+    "io/ioutil"
+    "strings"
+    "log"
 )
 
 type BooksTab struct {
@@ -21,6 +22,7 @@ func newBooksTab(a *Apollo) *BooksTab {
             sortField: "title",
             status: "books",
             view: "passive",
+            additionalField: "author",
         },
     }
 
@@ -32,6 +34,7 @@ func newBooksTab(a *Apollo) *BooksTab {
 func (t *BooksTab) HandleKeyEvent(ev *termbox.Event) bool {
     switch ev.Ch {
     case 't':
+        t.fetchGoogleBooksTags()
         return true
     }
 
@@ -43,6 +46,66 @@ func (t *BooksTab) Query(query string) {
     t.query(query)
 
     if query[0] != ':' && t.a.c.get("autotag") == "true" {
+        t.fetchGoogleBooksTags()
         t.a.inputActive = false
+    }
+}
+
+type GoogleBooksInfo struct {
+    Title string
+    Authors []string
+    PublishedDate string
+}
+
+type GoogleBooksEntry struct {
+    Id string
+    VolumeInfo GoogleBooksInfo
+}
+
+type GoogleBooksData struct {
+    Items []GoogleBooksEntry
+}
+
+func (t *BooksTab) fetchGoogleBooksTags() {
+    title := strings.Replace(t.slice[t.cursor].Title, " ", "+", -1)
+    url := "https://www.googleapis.com/books/v1/volumes?q=" + title + "&projection=lite&printType=books&maxResults=10"
+    t.a.logDebug(url)
+
+    res, err := http.Get(url)
+    if err != nil {
+        t.a.logError(err.Error())
+        return
+    }
+    defer res.Body.Close()
+    body, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+        t.a.logError(err.Error())
+        return
+    }
+
+    var data GoogleBooksData
+    err = json.Unmarshal(body, &data)
+    if err != nil {
+        t.a.logError(err.Error())
+        return
+    }
+
+    log.Print(data)
+
+    for i := 0; i < len(data.Items); i++ {
+        if i < 10 {
+            if len(data.Items[i].VolumeInfo.Authors) == 0 {
+                data.Items[i].VolumeInfo.Authors = append(data.Items[i].VolumeInfo.Authors, "")
+            }
+            t.search = append(t.search, Entry{
+                Title: data.Items[i].VolumeInfo.Title,
+                TagID: data.Items[i].Id,
+                Info1: data.Items[i].VolumeInfo.Authors[0],
+            })
+        }
+    }
+
+    if len(t.search) > 0 {
+        t.view = "tag"
     }
 }
